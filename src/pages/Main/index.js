becb1f353d2d382b5d1a4d26225056256a5a2439
Igor Souza <igorsouza.dev@gmail.com>
2019-11-04 12:49:44 -0300

    Added app header

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Text} from 'react-native';
 
+import Header from '~/components/Header';
+
 import {Container} from './styles';
 
 export default function Main() {
   return (
     <Container>
+      <Header />
       <Text>Main</Text>
     </Container>
   );
