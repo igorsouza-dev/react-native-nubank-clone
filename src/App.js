9f041e4fa3ea572145bf731c26c9acead4360d50
Igor Souza <igorsouza.dev@gmail.com>
2019-11-04 11:48:39 -0300

    Added navigation and main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Nubank</Text>
-    </View>
-  );
+  return <Routes />;
 }
